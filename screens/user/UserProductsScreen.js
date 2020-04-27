b3cfbbce3a9cfa81e251f412e870f42e701b5fbb
zakve <zaklas.martin@gmail.com>
2020-04-27 19:32:31 +0200

    Update UserProductsScreen with item list

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,13 +1,60 @@
 import React from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
+import { Button, Icon } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { CustomHeaderButton, Item } from "../../components/UI/HeaderButton";
+
+// Components
+import ProductItem from "../../components/shop/ProductItem";
+
+// Constants
+import Colors from "../../constants/Colors";
 
 const UserProductsScreen = props => {
     const userProducts = useSelector(state => state.products.userProducts);
 
     return (
-        <Text>Admin view</Text>
+        <FlatList
+            data={userProducts}
+            keyExtractor={item => item.id}
+            renderItem={
+                itemData =>
+                    <ProductItem
+                        imageUrl={itemData.item.imageUrl}
+                        title={itemData.item.title}
+                        price={itemData.item.price}
+                        onSelect={() => {
+                        }}
+                        onAddToCart={() => {
+                            dispatch(cartActions.addToCart(itemData.item)),
+                                Snackbar.show({
+                                    text: 'Item added to Cart',
+                                    duration: Snackbar.LENGTH_SHORT,
+                                });
+                        }}
+                    >
+                        <Button
+                            title='Delete'
+                            type="clear"
+                            titleStyle={styles.buttonTitle}
+                            onPress={() => {
+                            }}
+                        />
+                        <Button
+                            title='Edit '
+                            buttonStyle={styles.button}
+                            icon={
+                                <Icon
+                                    name="edit"
+                                    size={20}
+                                    color="white"
+                                />}
+                            iconRight={true}
+                            onPress={() => { }}
+                        />
+                    </ProductItem>
+            }
+        />
     )
 }
 
@@ -27,5 +74,15 @@ UserProductsScreen.navigationOptions = navData => {
         ),
     }
 }
+
+const styles = StyleSheet.create({
+    button: {
+        paddingHorizontal: 10,
+        backgroundColor: Colors.primary
+    },
+    buttonTitle: {
+        color: "red"
+    }
+});
 
 export default UserProductsScreen;
