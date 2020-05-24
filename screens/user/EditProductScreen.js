2611e8f70cc9b68c4bca7be476ce8de55e584ef7
zakve <zaklas.martin@gmail.com>
2020-05-24 14:46:09 +0200

    Add KeyboardAvoidingView to form

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { View, StyleSheet, ScrollView, Alert } from "react-native";
-import { Text, Input, Button } from "react-native-elements";
+import React, { useEffect } from "react";
+import { View, StyleSheet, ScrollView, KeyboardAvoidingView } from "react-native";
+import { Input } from "react-native-elements";
 import { CustomHeaderButton, Item } from "../../components/UI/HeaderButton";
 import { useSelector, useDispatch } from "react-redux";
 import * as productsActions from "../../store/actions/products";
@@ -22,86 +22,92 @@ const EditProductScreen = props => {
     }
 
     return (
-        <ScrollView style={styles.background}>
-            <Formik
-                initialValues={{
-                    title: editedProduct ? editedProduct.title : '',
-                    imageUrl: editedProduct ? editedProduct.imageUrl : '',
-                    price: editedProduct ? editedProduct.price : '',
-                    description: editedProduct ? editedProduct.description : ''
-                }}
-                onSubmit={values => submitHandler(values)}
-                validationSchema={yup.object().shape({
-                    title: yup
-                        .string()
-                        .required('Please, provide title!'),
-                    imageUrl: yup
-                        .string()
-                        .required()
-                        .url(),
-                    price: yup
-                        .number()
-                        .required()
-                        .moreThan(0),
-                    description: yup
-                        .string()
-                        .required(),
-                })}
-            >
-                {({ values, handleChange, errors, handleBlur, setFieldTouched, touched, handleSubmit }) => {
-                    useEffect(() => {
-                        props.navigation.setParams({ submit: handleSubmit })
-                    }, [handleSubmit])
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior='padding'
+            keyboardVerticalOffset={100}
+        >
+            <ScrollView style={styles.background}>
+                <Formik
+                    initialValues={{
+                        title: editedProduct ? editedProduct.title : '',
+                        imageUrl: editedProduct ? editedProduct.imageUrl : '',
+                        price: editedProduct ? editedProduct.price : '',
+                        description: editedProduct ? editedProduct.description : ''
+                    }}
+                    onSubmit={values => submitHandler(values)}
+                    validationSchema={yup.object().shape({
+                        title: yup
+                            .string()
+                            .required('Please, provide title!'),
+                        imageUrl: yup
+                            .string()
+                            .required()
+                            .url(),
+                        price: yup
+                            .number()
+                            .required()
+                            .moreThan(0),
+                        description: yup
+                            .string()
+                            .required(),
+                    })}
+                >
+                    {({ values, handleChange, errors, handleBlur, setFieldTouched, touched, handleSubmit }) => {
+                        useEffect(() => {
+                            props.navigation.setParams({ submit: handleSubmit })
+                        }, [handleSubmit])
 
-                    return (
-                        <View style={styles.screen}>
-                            <Input
-                                label='Title'
-                                value={values.title}
-                                onChangeText={handleChange('title')}
-                                onBlur={handleBlur('title')}
-                                containerStyle={styles.inputContainer}
-                                keyboardType='default'
-                                autoCapitalize='sentences'
-                                autoCorrect
-                                errorMessage={(touched.title && errors.title) ? errors.title : ''}
-                            />
-                            <Input
-                                label='Image URL'
-                                value={values.imageUrl}
-                                onChangeText={handleChange('imageUrl')}
-                                onBlur={handleBlur('imageUrl')}
-                                containerStyle={styles.inputContainer}
-                                errorMessage={(touched.imageUrl && errors.imageUrl) ? errors.imageUrl : ''}
-                            />
-                            {
-                                !editedProduct &&
+                        return (
+                            <View style={styles.screen}>
                                 <Input
-                                    label='Price'
-                                    value={values.price}
-                                    onChangeText={handleChange('price')}
-                                    onBlur={handleBlur('price')}
+                                    label='Title'
+                                    value={values.title}
+                                    onChangeText={handleChange('title')}
+                                    onBlur={handleBlur('title')}
                                     containerStyle={styles.inputContainer}
-                                    keyboardType='decimal-pad'
-                                    errorMessage={(touched.price && errors.price) ? errors.price : ''}
+                                    keyboardType='default'
+                                    autoCapitalize='sentences'
+                                    autoCorrect
+                                    errorMessage={(touched.title && errors.title) ? errors.title : ''}
                                 />
-                            }
+                                <Input
+                                    label='Image URL'
+                                    value={values.imageUrl}
+                                    onChangeText={handleChange('imageUrl')}
+                                    onBlur={handleBlur('imageUrl')}
+                                    containerStyle={styles.inputContainer}
+                                    errorMessage={(touched.imageUrl && errors.imageUrl) ? errors.imageUrl : ''}
+                                />
+                                {
+                                    !editedProduct &&
+                                    <Input
+                                        label='Price'
+                                        value={values.price}
+                                        onChangeText={handleChange('price')}
+                                        onBlur={handleBlur('price')}
+                                        containerStyle={styles.inputContainer}
+                                        keyboardType='decimal-pad'
+                                        errorMessage={(touched.price && errors.price) ? errors.price : ''}
+                                    />
+                                }
 
-                            <Input
-                                label='Description'
-                                value={values.description}
-                                onChangeText={handleChange('description')}
-                                onBlur={handleBlur('description')}
-                                containerStyle={styles.inputContainer}
-                                keyboardType='default'
-                                autoCapitalize='sentences'
-                                errorMessage={(touched.description && errors.description) ? errors.description : ''}
-                            />
-                        </View>
-                    )
-                }}
-            </Formik>
-        </ScrollView>
+                                <Input
+                                    label='Description'
+                                    value={values.description}
+                                    onChangeText={handleChange('description')}
+                                    onBlur={handleBlur('description')}
+                                    containerStyle={styles.inputContainer}
+                                    keyboardType='default'
+                                    autoCapitalize='sentences'
+                                    errorMessage={(touched.description && errors.description) ? errors.description : ''}
+                                />
+                            </View>
+                        )
+                    }}
+                </Formik>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 
