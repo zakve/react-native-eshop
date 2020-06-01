c820aed0530104a460c6abb14777226551038818
zakve <zaklas.martin@gmail.com>
2020-06-01 08:41:12 +0200

    Add storing data on server

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,14 +7,33 @@ export const deleteProduct = productId => {
 }
 
 export const createProduct = (title, description, imageUrl, price) => {
-    return {
-        type: CREATE_PRODUCT,
-        productData: {
-            title,
-            description,
-            imageUrl,
-            price
-        }
+    return async dispatch => {
+        // async code here
+        const response = await fetch('https://react-native-eshop.firebaseio.com/products.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                imageUrl,
+                price
+            })
+        })
+
+        const resData = await response.json();
+
+        dispatch({
+            type: CREATE_PRODUCT,
+            productData: {
+                id: resData.name,
+                title,
+                description,
+                imageUrl,
+                price
+            }
+        })
     }
 }
 
