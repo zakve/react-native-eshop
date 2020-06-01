be53fa79f0a237b8ed36e77d3ee36541bff05ef6
zakve <zaklas.martin@gmail.com>
2020-06-01 14:18:19 +0200

    Add PATCH and DELETE product

--- store/actions/products.js
+++ store/actions/products.js
@@ -37,7 +37,15 @@ export const fetchProducts = () => {
 }
 
 export const deleteProduct = productId => {
-    return { type: DELETE_PRODUCT, pid: productId }
+    return async dispatch => {
+        await fetch(
+            `https://react-native-eshop.firebaseio.com/products/${productId}.json`,
+            {
+                method: 'DELETE'
+            })
+
+        dispatch({ type: DELETE_PRODUCT, pid: productId })
+    }
 }
 
 export const createProduct = (title, description, imageUrl, price) => {
@@ -72,13 +80,29 @@ export const createProduct = (title, description, imageUrl, price) => {
 }
 
 export const updateProduct = (id, title, description, imageUrl) => {
-    return {
-        type: UPDATE_PRODUCT,
-        pid: id,
-        productData: {
-            title,
-            description,
-            imageUrl
-        }
+    return async dispatch => {
+        await fetch(
+            `https://react-native-eshop.firebaseio.com/products/${id}.json`,
+            {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    title,
+                    description,
+                    imageUrl
+                })
+            })
+
+        dispatch({
+            type: UPDATE_PRODUCT,
+            pid: id,
+            productData: {
+                title,
+                description,
+                imageUrl
+            }
+        })
     }
 }
